05245ae5c319ebfa5c4e0eff850050584e5f1088
urias-cloud <ury7292@gmail.com>
2025-06-10 00:14:06 -0300

    test pocion

--- tests/test_pocion.py
+++ tests/test_pocion.py
@@ -2,7 +2,7 @@ import time
 from modelos.pocion import Pocion
 
 def test_explosion_despues_tiempo():
-    pocion = Pocion(0, 0, timer=1)
-    time.sleep(1.1)
-    pocion.actualizar()
-    assert pocion.explotada == True
+    pocion = Pocion(0, 0, timer=1) #creamos la pocion para que explote en 1 segundo
+    pocion.tiempo_inicial -= 1.5 #le restamos a tiempo inicial(cuando se coloco) mas de 1 segundo
+    pocion.actualizar() #al llamar la funcion actualizar, la bomba ya exploto
+    self.assertTrue(pocion.explotada)
