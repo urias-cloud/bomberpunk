359d622689e32c534925a44d40bf7b5f421fa9a2
urias-cloud <ury7292@gmail.com>
2025-06-10 00:20:44 -0300

    test pocion corregido

--- tests/test_pocion.py
+++ tests/test_pocion.py
@@ -1,8 +1,15 @@
 import time
 from modelos.pocion import Pocion
 
-def test_explosion_despues_tiempo():
-    pocion = Pocion(0, 0, timer=1) #creamos la pocion para que explote en 1 segundo
-    pocion.tiempo_inicial -= 1.5 #le restamos a tiempo inicial(cuando se coloco) mas de 1 segundo
-    pocion.actualizar() #al llamar la funcion actualizar, la bomba ya exploto
-    self.assertTrue(pocion.explotada)
+class TestPocion(unittest.TestCase):
+    def test_explosion_despues_tiempo(self):
+        pocion = Pocion(0, 0, timer=1) #creamos la pocion para que explote en 1 segundo
+        pocion.tiempo_inicial -= 1.5 #le restamos a tiempo inicial(cuando se coloco) mas de 1 segundo
+        pocion.actualizar() #al llamar la funcion actualizar, la bomba ya exploto
+        self.assertTrue(pocion.explotada)
+
+    def test_bomba_no_exploto(self):
+        pocion = Pocion(0,0, timer = 5) #creamos la pocion con un tiempo de 5
+        pocion.tiempo_inicial -= 4 #le restamos cuatro osea que queda en uno
+        pocion.actualizar() #como esta en uno llamamos al metodo actualizar y tendria que salir falso
+        self.assertFalse(pocion.explotada)
